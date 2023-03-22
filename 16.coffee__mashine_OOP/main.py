6781faede5dfac38db8a60c43cc6345ee5fb4a1c
Aleks_work <alemarkhel@yandex.ru>
2023-03-22 08:42:41 +0300

    16.OOP coffee_machine finish

--- 16.coffee__mashine_OOP/main.py
+++ 16.coffee__mashine_OOP/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+# menu_item = MenuItem()
+
+
+
+is_on = True
+while is_on:
+    menu_choice = menu.get_items()
+    drink = input(f"What would you like? ({menu_choice}): ")
+    if drink == "off":
+        is_on = False
+    elif drink == "report":
+        coffee_maker.report()
+        money_machine.report()
+        is_on = False
+    else:
+        drink_ordered = menu.find_drink(drink)
+        if coffee_maker.is_resource_sufficient(drink_ordered) and money_machine.make_payment(drink_ordered.cost):
+            coffee_maker.make_coffee(drink_ordered)
