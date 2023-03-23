63814f161e1f9a86a3b462804d59a9badbd24238
Aleks_work <alemarkhel@yandex.ru>
2023-03-23 09:13:42 +0300

    17.quize. init class Question

--- 17.Quize/main.py
+++ 17.Quize/main.py
@@ -1,27 +1,7 @@
-class User:
-    def __init__(self, user_id, user_name):
-        self.id = user_id
-        self.username = user_name
-        self.followers = 0
-        self.following = 0
+from question_model import Question
+from data import question_data
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-
-
-user_1 = User("001", "aleks")
-user_2 = User("002", "anna")
-user_3 = User("003", "john")
-user_4 = User("004", "nicki")
-
-# print(user_1.id)
-# print(user_2.username)
-# print(user_4.followers)
-
-user_1.follow(user_2)
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
-
+question_bank = []
+for i_question in range(len(question_data)):
+    question_bank.append(Question(question_data[i_question]["text"], question_data[i_question]["answer"]))
+print(question_bank[1].answer)
