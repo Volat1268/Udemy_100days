8c62b3368df9d2050abac3a492de46867fe1703c
aleksandr_markhel <volatpy@gmail.com>
2023-03-24 00:12:01 +0300

    init class Question and class QuizBrain

--- 17.Quize/main.py
+++ 17.Quize/main.py
@@ -2,16 +2,9 @@ from question_model import Question
 from data import question_data
 
 question_bank = []
-for i_question in range(len(question_data)):
-    question_bank.append(Question(question_data[i_question]["text"], question_data[i_question]["answer"]))
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
 
 
-def plus(a, b):
-    return a + b
 
 
-print(plus(12, 12))
-
-
-def minus(a, b):
-    return a - b
---
+++ 17.Quize/quiz_brain.py
@@ -0,0 +1,8 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self, u_answer):
+        self.u_answer = input()
+
