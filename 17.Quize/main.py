229efab265ac0c25042c13732665a3b0d0bd43d7
aleksandr_markhel <volatpy@gmail.com>
2023-03-23 19:48:06 +0300

    add def plus

--- 17.Quize/main.py
+++ 17.Quize/main.py
@@ -4,4 +4,9 @@ from data import question_data
 question_bank = []
 for i_question in range(len(question_data)):
     question_bank.append(Question(question_data[i_question]["text"], question_data[i_question]["answer"]))
-print(question_bank[1].answer)
+
+
+def plus(a, b):
+    return a + b
+
+print(plus(12, 12))
