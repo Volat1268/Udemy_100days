6391ac80e47791c8a4f4a346cd61eaa90dc38be9
Aleks_work <alemarkhel@yandex.ru>
2023-03-24 16:00:18 +0300

    QuizBrain method

--- 17.Quize/main.py
+++ 17.Quize/main.py
@@ -1,10 +1,15 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_text = question["text"]
+    question_answer = question["answer"]
+    question_bank.append(Question(question_text, question_answer))
 
 
+quize = QuizBrain(question_bank)
+quize.next_question()
 
 
--- 17.Quize/quiz_brain.py
+++ 17.Quize/quiz_brain.py
@@ -3,6 +3,9 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
-    def next_question(self, u_answer):
-        self.u_answer = input()
+    def next_question(self):
+        next_question = self.question_list[self.question_number]
+        u_answer = input(f"Q.{self.question_number + 1}: {next_question.text} (True or False): ").lower()
+
+
 
