87906793fc4777607ed4c2640ca2bddcdb936f48
aleksandr_markhel <volatpy@gmail.com>
2023-03-25 22:14:27 +0300

    17.Quiz finish

--- 17.Quize/main.py
+++ 17.Quize/main.py
@@ -4,12 +4,18 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     question_bank.append(Question(question_text, question_answer))
 
 
-quize = QuizBrain(question_bank)
-quize.next_question()
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+quiz.show_final_score()
+
+
+
+
 
 
