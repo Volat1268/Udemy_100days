4c5ab66c3d61441bfb46076b62c993dd3e2692b2
aleksandr_markhel <volatpy@gmail.com>
2023-03-26 17:48:12 +0300

    turtle start

---
+++ 18.turtle/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
