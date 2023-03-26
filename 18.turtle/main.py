6f5508c5fda97b4f70eb59ef001458c4ca08eb6a
aleksandr_markhel <volatpy@gmail.com>
2023-03-26 21:08:18 +0300

    4 challenges finish

--- 18.turtle/main.py
+++ 18.turtle/main.py
@@ -1,31 +1,44 @@
 from turtle import Turtle, Screen
+import random
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
+tim = Turtle()
+tim.shape("classic")
 
-
-
-
-
-
-
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
 
 
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
 
+# colors = [
+#     "medium spring green", "gold", "orange red", "yellow green", "rosy brown", "blue3", "black", "DarkOrange3",
+#     "HotPink1", "gray10", "LightPink", "HotPink2", "LightSeaGreen", "red", "yellow", "black", "salmon1",
+#     "yellow4", "purple3", "snow3", "turquoise", "YellowGreen", "tan1", "LimeGreen"
+# ]
+
+colors = [
+    "yellow", "black", "red", "blue", "green"
+]
 
 
+def draw_shapes(start_shape, finish_shape):
+    angle = start_shape
+    for i in range(finish_shape - start_shape + 1):
+        current_color = random.choice(colors)
+        for _ in range(angle):
+            tim.pencolor(current_color)
+            tim.forward(100)
+            tim.right(360 / angle)
+        angle += 1
 
 
-
-
-
-
-
-
-
-
-
+draw_shapes(3, 10)
 
 screen = Screen()
 screen.exitonclick()
