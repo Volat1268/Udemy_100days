ecc21ca3ea9e5eec13307e11682c7cebc6995e7d
Aleks_work <alemarkhel@yandex.ru>
2023-04-11 16:51:14 +0300

    method move for Snake

--- 20.snake/snake.py
+++ 20.snake/snake.py
@@ -1,8 +1,24 @@
+from turtle import Turtle, Screen
+import time
+
+START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+screen = Screen()
+
 class Snake:
-    def __int__(self, start_position):
-        self.color = "white"
-        self
-        self.start_position = start_position
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
 
+    def create_snake(self):
+        for position in START_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
-
+    def mov(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
