e478f7d190530cd9c7ab2e8f81999ba1522a3471
Aleks_work <alemarkhel@yandex.ru>
2023-04-13 17:37:36 +0300

    methods up, down, left, right

--- 20.snake/snake.py
+++ 20.snake/snake.py
@@ -1,16 +1,18 @@
 from turtle import Turtle, Screen
 import time
 
-START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-screen = Screen()
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for position in START_POSITIONS:
+        for position in STARTING_POSITIONS:
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
@@ -22,3 +24,16 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
+        self.head.fd(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
--- 20.snake/snake_main.py
+++ 20.snake/snake_main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 
 screen = Screen()
@@ -7,34 +8,22 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-segments = []
-start_positions = [(0, 0), (-20, 0), (-40, 0)]
-for position in start_positions:
-    new_segment = Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(position)
-    segments.append(new_segment)
 
 
 game_is_on = True
-# while game_is_on:
-#     for seg in segments:
-#         seg.penup()
-#         seg.fd(20)
-
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.4)
+    snake.mov()
 
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].fd(20)
 
 
 
