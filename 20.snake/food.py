266c7f51de9a1d5c4e4d0b0a5d15ecb067d5fa73
Aleks_work <alemarkhel@yandex.ru>
2023-04-21 15:12:22 +0300

    class scoreboard create

--- 20.snake/food.py
+++ 20.snake/food.py
@@ -9,9 +9,7 @@ class Food(Turtle):
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.penup()
         self.speed("fastest")
-        pos_x = random.randint(-280, 280)
-        pos_y = random.randint(-280, 280)
-        self.goto(pos_x, pos_y)
+        self.refresh()
 
     def refresh(self):
         pos_x = random.randint(-280, 280)
