cc3d9f1060dbb73b71c562adc00dc3ea2f67e5ab
aleksandr_markhel <volatpy@gmail.com>
2023-04-28 23:56:31 +0300

    class ball start creating

--- 22.pong/paddle.py
+++ 22.pong/paddle.py
@@ -2,21 +2,23 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.penup()
         self.setheading(90)
         self.speed(0)
-        self.goto(350, 0)
+        self.goto(position)
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_len=5)
 
     def move_up(self):
-        self.forward(20)
+        new_ycor = self.ycor() + 20
+        self.sety(new_ycor)
 
     def move_down(self):
-        self.backward(20)
+        new_ycor = self.ycor() - 20
+        self.sety(new_ycor)
 
     def running(self):
         self.forward(10)
--- 22.pong/pong_main.py
+++ 22.pong/pong_main.py
@@ -1,22 +1,29 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("My Pong game")
+screen.tracer(0)
 screen.listen()
 
-paddle_right = Paddle()
-paddle_left = Paddle()
-paddle_left.goto(-350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
-screen.onkey(paddle_left.move_up, "Up")
-screen.onkey(paddle_left.move_down, "Down")
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
 
 
 game_is_on = True
 while game_is_on:
-    paddle_right.running()
+    screen.update()
+    # l_paddle.running()
 
 
 
