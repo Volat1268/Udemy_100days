ab51d5111157d0cef345f88f6da62f791ed9015e
aleksandr_markhel <volatpy@gmail.com>
2023-05-04 21:29:42 +0300

    class player create

--- 23.turtle-crossing/car_manager.py
+++ 23.turtle-crossing/car_manager.py
@@ -1,7 +1,15 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+start_xcor = random.randrange(100, 250)
+start_ycor = random.randrange(-250, 250)
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        al_cars = []
+
+
--- 23.turtle-crossing/main.py
+++ 23.turtle-crossing/main.py
@@ -1,14 +1,36 @@
 import time
 from turtle import Screen
-from player import Player
+from player import *
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Cross the road!")
 screen.tracer(0)
+screen.listen()
+player = Player()
+screen.onkey(player.player_move, "Up")
+car_1 = CarManager()
+car_2 = CarManager()
+# car_4 = CarManager()
+# car_5 = CarManager()
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.ycor() >= FINISH_LINE_Y:
+        print("Finish")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
