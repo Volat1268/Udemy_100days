9d0c448dd472ce22df17b64e50d93d59074f706f
aleksandr_markhel <volatpy@gmail.com>
2023-05-10 16:17:22 +0300

    file path

--- 24.snake-2/scoreboard.py
+++ 24.snake-2/scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
+
 class Scoreboard(Turtle):
 	def __init__(self):
 		super().__init__()
@@ -9,7 +10,8 @@ class Scoreboard(Turtle):
 		self.hideturtle()
 		self.goto(0, 270)
 		self.score = 0
-		self.heigh_score = 0
+		with open("./24.snake-2/data.txt") as data:
+			self.heigh_score = int(data.read())
 		self.update_scoreboard()
 
 
@@ -21,6 +23,8 @@ class Scoreboard(Turtle):
 	def reset(self):
 		if self.score > self.heigh_score:
 			self.heigh_score = self.score
+			with open("./24.snake-2/data.txt", mode="w") as data:
+				data.write(str(self.heigh_score))
 		self.score = 0
 		self.update_scoreboard()
 
